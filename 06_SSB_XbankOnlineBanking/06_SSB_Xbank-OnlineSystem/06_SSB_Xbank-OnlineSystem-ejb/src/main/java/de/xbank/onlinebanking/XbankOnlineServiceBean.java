5dfeec976d4495f314c58c1ddb8fd285b551b135
Matthias Kutz <kutz.matthias@gmail.com>
2016-04-15 13:35:16 +0200

    Verzeichnisse verschoben

---
+++ 06_SSB_XbankOnlineBanking/06_SSB_Xbank-OnlineSystem/06_SSB_Xbank-OnlineSystem-ejb/src/main/java/de/xbank/onlinebanking/XbankOnlineServiceBean.java
@@ -0,0 +1,114 @@
+package de.xbank.onlinebanking;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.DependsOn;
+import javax.ejb.Remote;
+import javax.ejb.Remove;
+import javax.ejb.Stateful;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.jboss.logging.Logger;
+
+import de.xbank.banking.AccountRegistry;
+import de.xbank.common.Account;
+import de.xbank.common.Customer;
+import de.xbank.common.NoSessionException;
+import de.xbank.common.XbankOnlineService;
+import de.xbank.customer.CustomerRegistry;
+
+/**
+ * @author Thoene Diese Stateless Session Bean implementiert das fuer das
+ *         OnlineBanking bereitgestellte Interface.
+ *
+ */
+@Stateful
+@Remote(XbankOnlineService.class)
+public class XbankOnlineServiceBean implements XbankOnlineService {
+
+	private static final Logger logger = Logger.getLogger(XbankOnlineServiceBean.class);
+
+	private Customer user;
+
+	private CustomerRegistry customerRegistry;
+
+	private AccountRegistry accountRegistry;
+
+	@PostConstruct
+	public void init() throws NamingException {
+		Context context = new InitialContext();
+		customerRegistry = (CustomerRegistry) context.lookup(
+				"java:global/06_SSB_Xbank-OnlineSystem-ear/06_SSB_Xbank-OnlineSystem-ejb/CustomerRegistry!de.xbank.customer.CustomerRegistry");
+		accountRegistry = (AccountRegistry) context.lookup(
+				"java:global/06_SSB_Xbank-OnlineSystem-ear/06_SSB_Xbank-OnlineSystem-ejb/AccountRegistry!de.xbank.banking.AccountRegistry");
+	}
+
+	@Override
+	public boolean login(String username, String password) {
+		boolean success = false;
+		this.user = customerRegistry.findCustomerByName(username);
+		if (user != null && user.getPassword().equals(password)) {
+			success = true;
+			logger.info("Login erfolgreich.");
+		} else {
+			logger.info("Login fehlgeschlagen, da Kunde unbekannt oder Passwort falsch. username=" + username);
+		}
+		return success;
+	}
+
+	@Override
+	@Remove
+	public void logout() throws NoSessionException {
+		validateLogin();
+		logger.info("Logout erfolgreich.");
+	}
+
+	@Override
+	public BigDecimal getBalance(int accountID) throws NoSessionException {
+		BigDecimal result = null;
+		Account konto = user.getAccountById(accountID);
+		if (konto != null) {
+			result = konto.getBalance();
+		}
+		logger.info("Abfrage Saldo Konto " + accountID + " liefert: " + result);
+		return result;
+	}
+
+	@Override
+	public BigDecimal transfer(int fromAccount, int toAccount, BigDecimal amount) throws NoSessionException {
+		BigDecimal result = null;
+		Account source = user.getAccountById(fromAccount);
+		Account target = accountRegistry.findAccountById(toAccount);
+		if (source != null && target != null) {
+			source.decrease(amount);
+			target.increase(amount);
+			result = source.getBalance();
+		}
+		logger.info(" Ueberweisung von Konto " + fromAccount + " liefert: " + result);
+		return result;
+	}
+
+	@Override
+	public Set<Account> getMyAccounts() throws NoSessionException {
+		Set<Account> result = new HashSet<Account>();
+		result = user.getAccounts();
+		logger.info(" Abfrage eigener Konten liefert: " + result);
+		return result;
+	}
+
+	public String toString() {
+		return "Hello, I'm an instance of XbankOnlineServiceImpl!";
+	}
+
+	private void validateLogin() throws NoSessionException {
+		if (user == null) {
+			throw new NoSessionException("Bitte zunächst einen Login durchführen");
+		}
+	}
+
+}
---
+++ 06_SSB_XbankOnlineBanking/06_SSB_Xbank-OnlineSystem/06_SSB_Xbank-OnlineSystem-ejb/src/main/java/de/xbank/session/UserSession.java
@@ -0,0 +1,32 @@
+package de.xbank.session;
+
+import de.xbank.common.Customer;
+
+public class UserSession {
+	
+	private static int lastID = 0;
+	
+	private final String sessionID;
+	private final Customer user;
+	
+	public UserSession(Customer user) {
+		lastID++;
+		this.sessionID = "" + lastID;
+		this.user = user;
+		SessionRegistry.getInstance().addSession(this);
+	}
+
+	public String getSessionID() {
+		return sessionID;
+	}
+
+	public Customer getUser() {
+		return user;
+	}
+	
+	@Override
+	public String toString() {
+		return "["+this.sessionID+","+this.getUser().getUserName()+"]";
+	}
+		
+}
